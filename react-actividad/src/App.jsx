2384d4786673589044c232140bf047e176d15d77
agustinbarbero <agustinbarbero932@gmail.com>
2025-09-29 08:54:48 -0300

    Subiendo Propuesta 3

--- react-actividad/src/App.jsx
+++ react-actividad/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Producto from "./components/Producto";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>Lista de Productos (Propuesta 3)</h2>
+
+      <Producto
+        name="Teclado mecanico"
+        price={40000}
+        discount={25}
+        category="Tecnología"
+      />
+
+      <Producto
+        name="Botines de futbol"
+        price={40000}
+        category="Calzado"
+      />
+
+      <Producto
+        name="Mochila"
+        price={20000}
+        discount={10}
+        category="Accesorios"
+      />
+    </div>
+  );
 }
 
 export default App
